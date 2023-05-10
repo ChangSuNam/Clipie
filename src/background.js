e642cc9cfa40413f6cdfa4d5a69b0558ee9ebaee
ChangSuNam <changsunam@gmail.com>
2023-05-10 17:45:45 -0400

    changed to show most recent C/P first, added function to save to .txt, added option to delete history, fixed designs of the popup.html.

--- src/background.js
+++ src/background.js
@@ -1,26 +1,33 @@
-var curHistory = [];
-console.log('background.js loaded');
+let curHistory = [];
+//console.log('background.js loaded');
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-
-  console.log('Listener function triggered:', request);
-
+  //console.log('Listener function triggered:', request);
 
   if (request.type == "copy") {
     curHistory.push({action: "Copied", text: request.text, timestamp: new Date(request.timestamp).toLocaleString()});
     console.log('Item copied:', request.text);
-    //sendResponse({history: curHistory});
     console.log('History requested:', curHistory);
 
   } else if (request.type == "paste") {
     curHistory.push({action: "Pasted", text: request.text, timestamp: new Date(request.timestamp).toLocaleString()});
     console.log('Item pasted:', request.text);
-    //sendResponse({history: curHistory});
     console.log('History requested:', curHistory);
-  }
+  } 
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.type == "getHistory") {
     sendResponse({history: curHistory});
   }
+  
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.type == "deleteHistory") {
+    curHistory = [];
+    console.log("history data cleared.")
+    sendResponse({history: curHistory});
+  }
+  
+});
+
--- src/popup.js
+++ src/popup.js
@@ -1,30 +1,30 @@
-// chrome.runtime.sendMessage({action: "getHistory"}, function(response) {
-//   var historyList = document.getElementById("history");
-//   for (var i = 0; i < response.history.length; i++) {
-//     var item = document.createElement("li");
-//     item.textContent = response.history[i].text + " (" + response.history[i].timestamp + ")";
-//     historyList.appendChild(item);
-//   }
-// });
-
 var historyList = document.getElementById("historyList");
 
+//get current history
 chrome.runtime.sendMessage({type: "getHistory"}, function(response) {
   var history = response.history;
 
-  for (var i = 0; i < history.length; i++) {
+  for (var i = history.length-1; i >= 0; i--) {
+    //List of copy and pastes
     var item = history[i];
     var listItem = document.createElement("li");
-    listItem.textContent = item.action + ": '" + item.text + "' at [" + item.timestamp + "]";
+    var textSpan = document.createElement("span");
+    textSpan.innerHTML = item.text;
     if (item.action === "Copied") {
-      listItem.style.color = "blue";
+      textSpan.classList.add("copiedText");
     } else if (item.action === "Pasted") {
-      listItem.style.color = "red";
+      textSpan.classList.add("pastedText");
     }
+  
+    listItem.innerHTML = item.action + ": '";
+    listItem.appendChild(textSpan);
+    listItem.innerHTML += "' @ [" + item.timestamp + "]";
 
+    //create button that will allow user to copy what's on the clipboard
     var copyButton = document.createElement("button");
     copyButton.textContent = "Copy";
-    copyButton.dataset.text = item.text; // Store the text to be copied in a custom data attribute
+    copyButton.dataset.text = item.text; 
+    copyButton.classList.add("copyButton");
     copyButton.addEventListener("click", function(event) {
       var textToCopy = event.target.dataset.text;
       navigator.clipboard.writeText(textToCopy).then(function() {
@@ -37,4 +37,53 @@ chrome.runtime.sendMessage({type: "getHistory"}, function(response) {
     listItem.appendChild(copyButton);
     historyList.appendChild(listItem);
   }
+
+
+
+  //button to save current history
+  var saveButton = document.getElementById("txtSaveButton");
+  // Add event listener to the save button
+  saveButton.addEventListener("click", function() {
+    //console.log("save button clicked")
+    var textToSave = "";
+    for (var i = history.length-1; i >= 0; i--) {
+      var item = history[i];
+      textToSave += item.action + ": " + item.text + " (" + item.timestamp + ")\n";
+    }
+
+    //blob object with the textToSave string
+    var blob = new Blob([textToSave], {type: "text/plain"});
+
+    // URL for the Blob object
+    var url = URL.createObjectURL(blob);
+
+    // link element- set its href to the URL
+    var link = document.createElement("a");
+    link.href = url;
+    link.download = "Clipie_clipBoard_history.txt";
+    link.textContent = "Download History";
+
+    // click to download
+    link.click();
+  });
+
+
+
+
+  // DeleteButton
+  var deleteButton = document.getElementById("historyDeleteButton");
+
+  deleteButton.addEventListener("click", function() {
+  // Clear the history array
+  history = [];
+  // Clear the history list in the popup
+  historyList.innerHTML = "";
+  // Send a message to the background script to clear the history in storage
+  chrome.runtime.sendMessage({type: "deleteHistory"}, function(response) {
+   // console.log('History deleted on popup.js side');
+  });
 });
+
+
+});
+
